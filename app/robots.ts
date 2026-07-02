ebee194f3b25342982c193553adb99ca18ce51dd
Mariuse <maruise@gmail.com>
2026-07-02 17:02:58 +0100

    fix: robots.txt - accès total pour tous les bots et IA

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "",
     },
     sitemap: "https://kamtech.online/sitemap.xml",
   };
